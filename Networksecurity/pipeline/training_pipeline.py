3e6d4facd7a4cb83f68dc742c1832f10905b41b3
Mahi <kapulurumahendrareddy@gmailcom>
2025-03-11 14:40:02 +0530

    Data Transformation completed

--- Networksecurity/pipeline/training_pipeline.py
+++ Networksecurity/pipeline/training_pipeline.py
@@ -59,8 +59,8 @@ class TrainingPipeline:
     def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
          try:
                data_transformation_config=DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
-               DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=data_transformation_config)
-               data_transformation_artifact= data_transformation_config.intiate_data_transformation()
+               data_transformation=DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=data_transformation_config)
+               data_transformation_artifact= data_transformation.initiate_data_transformation()
                return  data_transformation_artifact
          except Exception as e:
               raise NetworkSecurityException(e,sys)
@@ -89,7 +89,7 @@ class TrainingPipeline:
               print(data_ingestion_artifact)
 
               data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-              
+
               data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
               print(data_transformation_artifact)
          except Exception as e:
